952cf7d2639ddcd5468c477129f0930bc9cd621c
Agustin-Benenatti <agus.benenatti09@gmail.com>
2024-10-18 00:42:14 -0300

    modificacion
    Creacion de la clase Alojamiento, Pasaje y modificacion de la clase Paquete

--- src/modelo/Alojamiento.java
+++ src/modelo/Alojamiento.java
@@ -4,10 +4,114 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author aguse
  */
 public class Alojamiento {
+    private int id_alojamiento;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private boolean estado;
+    private String servicio;
+    private double importeDiario;
+    private Ciudad ciudadDestino;
+    private String tipoAlojamiento;
+
+    public Alojamiento(int id_alojamiento, LocalDate fechaInicio, LocalDate fechaFin, boolean estado, String servicio, double importeDiario, Ciudad ciudadDestino, String tipoAlojamiento) {
+        this.id_alojamiento = id_alojamiento;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.estado = estado;
+        this.servicio = servicio;
+        this.importeDiario = importeDiario;
+        this.ciudadDestino = ciudadDestino;
+        this.tipoAlojamiento = tipoAlojamiento;
+    }
+
+    public Alojamiento(LocalDate fechaInicio, LocalDate fechaFin, boolean estado, String servicio, double importeDiario, Ciudad ciudadDestino, String tipoAlojamiento) {
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.estado = estado;
+        this.servicio = servicio;
+        this.importeDiario = importeDiario;
+        this.ciudadDestino = ciudadDestino;
+        this.tipoAlojamiento = tipoAlojamiento;
+    }
+
+    public Alojamiento() {
+    }
+
+    public int getId_alojamiento() {
+        return id_alojamiento;
+    }
+
+    public void setId_alojamiento(int id_alojamiento) {
+        this.id_alojamiento = id_alojamiento;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
+
+    public double getImporteDiario() {
+        return importeDiario;
+    }
+
+    public void setImporteDiario(double importeDiario) {
+        this.importeDiario = importeDiario;
+    }
+
+    public Ciudad getCiudadDestino() {
+        return ciudadDestino;
+    }
+
+    public void setCiudadDestino(Ciudad ciudadDestino) {
+        this.ciudadDestino = ciudadDestino;
+    }
+
+    public String getTipoAlojamiento() {
+        return tipoAlojamiento;
+    }
+
+    public void setTipoAlojamiento(String tipoAlojamiento) {
+        this.tipoAlojamiento = tipoAlojamiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Alojamiento{" + "id_alojamiento=" + id_alojamiento + ", fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + ", estado=" + estado + ", servicio=" + servicio + ", importeDiario=" + importeDiario + ", ciudadDestino=" + ciudadDestino + ", tipoAlojamiento=" + tipoAlojamiento + '}';
+    }
+    
     
 }
--- src/modelo/Pasaje.java
+++ src/modelo/Pasaje.java
@@ -9,5 +9,74 @@ package modelo;
  * @author aguse
  */
 public class Pasaje {
+    private int id_pasaje;
+    private String tipoTransporte;
+    private double importe;
+    private Ciudad ciudadOrigen;
+    private boolean estado;
+
+    public Pasaje(int id_pasaje, String tipoTransporte, double importe, Ciudad ciudadOrigen, boolean estado) {
+        this.id_pasaje = id_pasaje;
+        this.tipoTransporte = tipoTransporte;
+        this.importe = importe;
+        this.ciudadOrigen = ciudadOrigen;
+        this.estado = estado;
+    }
+
+    public Pasaje(String tipoTransporte, double importe, Ciudad ciudadOrigen, boolean estado) {
+        this.tipoTransporte = tipoTransporte;
+        this.importe = importe;
+        this.ciudadOrigen = ciudadOrigen;
+        this.estado = estado;
+    }
+
+    public Pasaje() {
+    }
+
+    public int getId_pasaje() {
+        return id_pasaje;
+    }
+
+    public void setId_pasaje(int id_pasaje) {
+        this.id_pasaje = id_pasaje;
+    }
+
+    public String getTipoTransporte() {
+        return tipoTransporte;
+    }
+
+    public void setTipoTransporte(String tipoTransporte) {
+        this.tipoTransporte = tipoTransporte;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public Ciudad getCiudadOrigen() {
+        return ciudadOrigen;
+    }
+
+    public void setCiudadOrigen(Ciudad ciudadOrigen) {
+        this.ciudadOrigen = ciudadOrigen;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Pasaje{" + "id_pasaje=" + id_pasaje + ", tipoTransporte=" + tipoTransporte + ", importe=" + importe + ", ciudadOrigen=" + ciudadOrigen + ", estado=" + estado + '}';
+    }
+    
     
 }
