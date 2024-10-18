526a1aa10d86d38ecee78505c1007555746891cd
Agustin-Benenatti <agus.benenatti09@gmail.com>
2024-10-18 00:27:04 -0300

    Avances
    Modificación de la clase Paquete y creación de las clases Alojamiento y Pasaje

--- src/modelo/Paquete.java
+++ src/modelo/Paquete.java
@@ -10,20 +10,27 @@ package modelo;
  */
 public class Paquete {
   private int id_paquete;
-  private int id_ciudadOrigen;
-  private int id_ciudadDestino;
-  private int id_alojamiento;
-  private int id_pasaje;
-  
-  public Paquete(){           
-  }
+  private Ciudad ciudadOrigen;
+  private Ciudad ciudadDestino;
+  private Alojamiento alojamiento;
+  private Pasaje pasaje;
 
-    public Paquete(int id_paquete, int id_ciudadOrigen, int id_ciudadDestino, int id_alojamiento, int id_pasaje) {
+    public Paquete(int id_paquete, Ciudad ciudadOrigen, Ciudad ciudadDestino, Alojamiento alojamiento, Pasaje pasaje) {
         this.id_paquete = id_paquete;
-        this.id_ciudadOrigen = id_ciudadOrigen;
-        this.id_ciudadDestino = id_ciudadDestino;
-        this.id_alojamiento = id_alojamiento;
-        this.id_pasaje = id_pasaje;
+        this.ciudadOrigen = ciudadOrigen;
+        this.ciudadDestino = ciudadDestino;
+        this.alojamiento = alojamiento;
+        this.pasaje = pasaje;
+    }
+
+    public Paquete(Ciudad ciudadOrigen, Ciudad ciudadDestino, Alojamiento alojamiento, Pasaje pasaje) {
+        this.ciudadOrigen = ciudadOrigen;
+        this.ciudadDestino = ciudadDestino;
+        this.alojamiento = alojamiento;
+        this.pasaje = pasaje;
+    }
+
+    public Paquete() {
     }
 
     public int getId_paquete() {
@@ -34,42 +41,43 @@ public class Paquete {
         this.id_paquete = id_paquete;
     }
 
-    public int getId_ciudadOrigen() {
-        return id_ciudadOrigen;
+    public Ciudad getCiudadOrigen() {
+        return ciudadOrigen;
     }
 
-    public void setId_ciudadOrigen(int id_ciudadOrigen) {
-        this.id_ciudadOrigen = id_ciudadOrigen;
+    public void setCiudadOrigen(Ciudad ciudadOrigen) {
+        this.ciudadOrigen = ciudadOrigen;
     }
 
-    public int getId_ciudadDestino() {
-        return id_ciudadDestino;
+    public Ciudad getCiudadDestino() {
+        return ciudadDestino;
     }
 
-    public void setId_ciudadDestino(int id_ciudadDestino) {
-        this.id_ciudadDestino = id_ciudadDestino;
+    public void setCiudadDestino(Ciudad ciudadDestino) {
+        this.ciudadDestino = ciudadDestino;
     }
 
-    public int getId_alojamiento() {
-        return id_alojamiento;
+    public Alojamiento getAlojamiento() {
+        return alojamiento;
     }
 
-    public void setId_alojamiento(int id_alojamiento) {
-        this.id_alojamiento = id_alojamiento;
+    public void setAlojamiento(Alojamiento alojamiento) {
+        this.alojamiento = alojamiento;
     }
 
-    public int getId_pasaje() {
-        return id_pasaje;
+    public Pasaje getPasaje() {
+        return pasaje;
     }
 
-    public void setId_pasaje(int id_pasaje) {
-        this.id_pasaje = id_pasaje;
+    public void setPasaje(Pasaje pasaje) {
+        this.pasaje = pasaje;
     }
 
     @Override
     public String toString() {
-        return "Paquete{" + "id_paquete=" + id_paquete + ", id_ciudadOrigen=" + id_ciudadOrigen + ", id_ciudadDestino=" + id_ciudadDestino + ", id_alojamiento=" + id_alojamiento + ", id_pasaje=" + id_pasaje + '}';
+        return "Paquete{" + "id_paquete=" + id_paquete + ", ciudadOrigen=" + ciudadOrigen + ", ciudadDestino=" + ciudadDestino + ", alojamiento=" + alojamiento + ", pasaje=" + pasaje + '}';
     }
-    
+  
+  
   
 }
