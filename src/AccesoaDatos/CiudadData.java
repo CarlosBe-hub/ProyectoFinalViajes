a8b563877c049acd4496572174b0dc9a34a12d50
FloresEmiliano <emilianoflores335@gmail.com>
2024-10-24 02:42:01 -0300

    avances
    XD

--- src/AccesoaDatos/CiudadData.java
+++ src/AccesoaDatos/CiudadData.java
@@ -44,25 +44,25 @@ public class CiudadData {
             JOptionPane.showMessageDialog(null, "Error en acceder a la tabla ciudad");
         }
     }
-    
-    public Ciudad buscarCiudad(String ciudad){
-    
-        String sql =" SELECT * FROM ciudad WHERE nombre=?";
-        Ciudad c =null;
-        
+
+    public Ciudad buscarCiudad(String ciudad) {
+
+        String sql = " SELECT * FROM ciudad WHERE nombre=?";
+        Ciudad c = null;
+
         try {
             PreparedStatement ps = red.prepareStatement(sql);
             ps.setString(1, ciudad);
-            ResultSet rs =ps.executeQuery();
-            
-            if(rs.next()){
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
                 c = new Ciudad();
                 c.setId_ciudad(rs.getInt("id_ciudad"));
                 c.setNombre(ciudad);
                 c.setPais(rs.getString("pais"));
                 c.setEstado(rs.getBoolean("estado"));
                 c.setProvincia(rs.getString("provincia"));
-                
+
                 JOptionPane.showMessageDialog(null, "Se ha encontrado la ciudad");
             }
         } catch (SQLException e) {
@@ -70,69 +70,66 @@ public class CiudadData {
         }
         return c;
     }
-    
-    public List<String> listarPaises(){
-        String sql ="SELECT DISTINCT pais FROM ciudad";
+
+    public List<String> listarPaises() {
+        String sql = "SELECT DISTINCT pais FROM ciudad";
         List<String> pais = new ArrayList<>();
-        
+
         try {
             PreparedStatement ps = red.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()){
+
+            while (rs.next()) {
                 String a = rs.getString("pais");
                 pais.add(a);
                 JOptionPane.showMessageDialog(null, "lista de paises");
             }
-            
+
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla ciudad!");
         }
         return pais;
     }
-    
-    public List<Ciudad> listarPaisYProvincia(String pais, String provincia){
-        String sql= "SELECT id_ciudad, nombre FROM ciudad WHERE provincia=? AND pais=?";
+
+    public List<Ciudad> listarPaisYProvincia(String pais, String provincia) {
+        String sql = "SELECT id_ciudad, nombre FROM ciudad WHERE provincia=? AND pais=?";
         ArrayList<Ciudad> ciudad = new ArrayList<>();
-        
-        
+
         try {
             PreparedStatement ps = red.prepareStatement(sql);
             ps.setString(1, provincia);
             ps.setString(2, pais);
-            
+
             ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()){
-                Ciudad c = new  Ciudad();
+
+            while (rs.next()) {
+                Ciudad c = new Ciudad();
                 c.setId_ciudad(rs.getInt("id_ciudad"));
                 c.setNombre(rs.getString("nombre"));
                 c.setEstado(true);
                 c.setProvincia(provincia);
                 c.setPais(pais);
-                
+
                 ciudad.add(c);
-                
-               
+
             }
-            
-            
+
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error ListarPaisYProvincia");
         }
         return ciudad;
     }
-    
-    public List<Ciudad> listarProvinciaXPais(String pais){
+
+    public List<Ciudad> listarProvinciaXPais(String pais) {
         String sql = "SELECT id_ciudad, nombre FROM ciudad WHERE provincia =? AND pais =?";
         ArrayList<Ciudad> ciudad = new ArrayList<>();
-        
+
         try {
             PreparedStatement ps = red.prepareStatement(sql);
             ps.setString(1, pais);
             ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()){
+
+            while (rs.next()) {
                 String p = rs.getString("provincia");
                 Ciudad c = new Ciudad();
                 c.setProvincia(p);
@@ -143,4 +140,62 @@ public class CiudadData {
         return ciudad;
     }
 
+    public void eliminarCiudad(int id) {
+        String sql = "UPDATE ciudad SET estado = 0 WHERE id_ciudad =?";
+        try {
+            PreparedStatement ps = red.prepareStatement(sql);
+            ps.setInt(1, id);
+            int i = ps.executeUpdate();
+            if (i == 1) {
+                JOptionPane.showMessageDialog(null, "Se ha dado de Baja la Ciudad");
+
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Ciudad");
+
+        }
+    }
+
+    public void modificarCiudad(Ciudad ciudad) {
+        String sql = "UPDATE ciudad SET nombre = ?, pais = ?, provincia = ? WHERE id_ciudad = ?";
+        try {
+            PreparedStatement ps = red.prepareStatement(sql);
+            ps.setString(1, ciudad.getNombre());
+            ps.setString(2, ciudad.getPais());
+            ps.setString(3, ciudad.getProvincia());
+            ps.setInt(4, ciudad.getId_ciudad());
+            int o = ps.executeUpdate();
+            if (o == 1) {
+                JOptionPane.showMessageDialog(null, "Se ha modificado la Ciudad");
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Ciudad");
+        }
+    
+    }
+    
+    public Ciudad buscarCiudadporid(int id){
+    Ciudad c = null;
+    String sql = "SELECT nombre, pais, estado, provincia FROM ciudad WHERE id_ciudad = ?";
+    
+            try {
+             PreparedStatement ps = red.prepareStatement(sql);
+             ps.setInt(1, id);
+             ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    c = new Ciudad();
+                    c.setId_ciudad(id);
+                    c.setNombre(rs.getString("nombre"));
+                    c.setPais(rs.getString("pais"));
+                    c.setEstado(rs.getBoolean("estado"));
+                    c.setProvincia(rs.getString("provincia"));
+                    
+                }
+            
+        } catch (SQLException e) {
+        }
+       return c;       
+    }
 }
--- src/AccesoaDatos/PasajeData.java
+++ src/AccesoaDatos/PasajeData.java
@@ -6,6 +6,7 @@ package AccesoaDatos;
 
 import java.sql.*;
 import javax.swing.JOptionPane;
+import modelo.Ciudad;
 import modelo.Pasaje;
 
 /**
@@ -44,4 +45,44 @@ public class PasajeData {
         }
 
     }
+    public void eliminarPasaje(int id){
+    String sql = "DELETE FROM pasaje WHERE id_pasaje=?";
+        try { 
+            PreparedStatement ps = red.prepareStatement(sql);
+            ps.setInt(1, id);
+            int i = ps.executeUpdate();
+            if (i == 1) {
+                
+                 JOptionPane.showMessageDialog(null, "El Pasaje ha sido Eliminado");
+            }
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al Acceder a la Tabla Pasaje");
+        }
+    }
+    
+    public Pasaje buscarPasaje(int id){
+        Pasaje p = null;
+    String sql = "SELECT tipo_Transporte, importe, id_ciudadOrigen, estado FROM pasaje WHERE id_pasaje= ?";
+        try {
+            PreparedStatement ps = red.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+              p = new Pasaje();
+              CiudadData cd = new CiudadData();
+              Ciudad c = cd.buscarCiudadporid(rs.getInt("id_ciudadOrigen"));
+              p.setId_pasaje(id);
+              
+              p.setCiudadOrigen(c);
+              p.setImporte(rs.getDouble("importe"));
+              p.setTipoTransporte(rs.getString("tipo_Transporte"));
+              p.setEstado(rs.getBoolean("estado"));
+              
+            }
+            
+        } catch (SQLException e) {
+        }
+        return p;
+    } 
 }
